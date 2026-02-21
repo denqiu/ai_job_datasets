18aafcfccdd56a279f69a0dcb944cc62e7c371fa
Dennis Qiu <dennisqiu@yahoo.com>
2026-02-21 13:31:56 -0500

    Added file check to sourcing env

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source .env
+# In local environment, load env variables. In GitHub Actions, env variables are already loaded in workflow and .env doesn't exist because of .gitignore.
+# In short, if .env exists, source it, otherwise, assume remote and variables already loaded so no need to source remotely.
+[ -f .env ] && source .env
 docker image prune -f
 docker compose up --remove-orphans
 docker cp ${DOCKER_CONTAINER}:/app/numpy_ai_job_datasets.csv ./numpy_ai_job_datasets.csv
