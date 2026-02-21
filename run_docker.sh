5b562d4cb0297e488ee59b7d15eadebbc4d98bf0
Dennis Qiu <dennisqiu@yahoo.com>
2026-02-21 14:00:31 -0500

    Added error reporting

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,8 @@
 # In local environment, load env variables. In GitHub Actions, env variables are already loaded in workflow and .env doesn't exist because of .gitignore.
 # In short, if .env exists, source it, otherwise, assume remote and variables already loaded so no need to source remotely.
 [ -f .env ] && source .env
+# Stop script and report error if any command fails
+set -e
 docker image prune -f
 docker compose up --remove-orphans
 docker cp ${DOCKER_CONTAINER}:/app/numpy_ai_job_datasets.csv ./numpy_ai_job_datasets.csv
