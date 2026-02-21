bb10874d1d9adb53b4651be2473ca0bd502a4394
Dennis Qiu <dennisqiu@yahoo.com>
2026-02-21 18:13:29 -0500

    Moved image prune command from before docker compose up to after docker compose up

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,8 @@
 # In local environment, load env variables. In GitHub Actions, env variables are already loaded in workflow and .env doesn't exist because of .gitignore.
 # In short, if .env exists, source it, otherwise, assume remote and variables already loaded so no need to source remotely.
 [ -f .env ] && source .env
-docker image prune -f
+# docker compose up switches image to the latest one and replace previous image with <none> tag
 docker compose up --remove-orphans
+# Then finds images with <none> tag, known as dangling images. Does not work before docker compose up.
+docker image prune -f
 docker cp ${DOCKER_CONTAINER}:/app/numpy_ai_job_datasets.csv ./numpy_ai_job_datasets.csv
